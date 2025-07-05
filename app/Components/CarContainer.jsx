b6c4424aca1a20e91f13969b37aca5baeb794791
sonumehta-tech <sonukumark468@gmail.com>
2025-07-05 17:06:03 +0530

    Update: changed color and icon and discription

--- app/Components/CarContainer.jsx
+++ app/Components/CarContainer.jsx
@@ -81,7 +81,7 @@ export default function CarsByBudget() {
         ))}
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
         {carsData[activeTab]?.map((car) => (
           <div
             key={car.name}
